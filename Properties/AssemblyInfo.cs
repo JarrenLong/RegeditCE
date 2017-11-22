fbb2db7fb867d16dec047e1a817b7c1292798a9d
Jarren Long <jarrenlong@gmail.com>
2017-11-22 08:56:49 -0800

    Adding project to target compiling for WinCE, Pocket PC, and Win Mobile 5.0 Smartphone/Pocket PC platforms

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("RegeditCE")]
 [assembly: AssemblyDescription("Registry Editor for Windows CE devices")]
 [assembly: AssemblyConfiguration("")]
@@ -13,21 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (c) 2012-2018 Long Technical")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d5b78bd0-0a59-4456-a078-9c14972da66a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
